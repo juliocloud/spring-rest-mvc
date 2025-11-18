828fe7062458659ddaf68e7c0ae603692a23710d
juliocloud <juliocesarjuriolli@gmail.com>
2025-11-17 21:17:10 -0300

    feat: creating custom exception

---
+++ src/main/java/com/julio/spring6restmvc/exceptions/ApiError.java
@@ -0,0 +1,27 @@
+package com.julio.spring6restmvc.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ApiError {
+
+    private HttpStatus status;
+    private String message;
+    private List<String> errors;
+
+    public ApiError(HttpStatus status, String message, List<String> errors) {
+        this.status = status;
+        this.message = message;
+        this.errors = errors;
+    }
+
+    public ApiError(HttpStatus status, String message, String error) {
+        this.status = status;
+        this.message = message;
+        errors = Collections.singletonList(error);
+    }
+}
