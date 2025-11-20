b392c90d0119b140e3789db520748bd1d1c2a697
juliocloud <juliocesarjuriolli@gmail.com>
2025-11-20 09:56:17 -0300

    feat: enhancing exception handling

--- src/main/java/com/julio/spring6restmvc/controllers/NotFoundException.java
+++ src/main/java/com/julio/spring6restmvc/controllers/NotFoundException.java
@@ -1,5 +1,9 @@
 package com.julio.spring6restmvc.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "stuff was not found")
 public class NotFoundException extends RuntimeException {
     public NotFoundException() {
         super();
